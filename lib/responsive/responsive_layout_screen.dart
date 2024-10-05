2753d66fcaccc6ed85a4fccac0a60de0601f4d2b
Saleh Muhammad Mangrio <Salehmangrio114@gmail.com>
2024-10-05 05:09:24 -0700

    Add files via upload

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
